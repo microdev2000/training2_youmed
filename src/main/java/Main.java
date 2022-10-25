f38bfb20bcfce327ca08158a8708a66671016730
Vo Khac Bao <baovk@youmed.vn>
2022-10-25 11:53:45 +0700

    update code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,24 @@
 import io.vertx.core.Vertx;
-import youmed.api.router.ClazzRouter;
-import youmed.api.service.ClazzService;
+import vn.youmed.model.Limit;
+import vn.youmed.model.Speciality;
+import vn.youmed.model.Student;
+import vn.youmed.router.ClazzRouter;
+import vn.youmed.service.ClazzService;
+import vn.youmed.service.StudentService;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Vertx vertx = Vertx.factory.vertx();
 		vertx.deployVerticle(ClazzRouter.class.getName());
+		vertx.deployVerticle(Student.class.getName());
+		vertx.deployVerticle(Speciality.class.getName());
+		vertx.deployVerticle(Limit.class.getName());
+
 		vertx.deployVerticle(ClazzService.class.getName());
+		vertx.deployVerticle(StudentService.class.getName());
+		vertx.deployVerticle(Speciality.class.getName());
+		vertx.deployVerticle(Limit.class.getName());
 
 	}
 
